1a6e9a2aec38eed6319a4f0e7b7a92fb8322c842
wbrunovieira <wbrunovieira77@gmail.com>
2022-11-07 15:57:04 +0000

    added the input box to add participants

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -15,5 +15,13 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FFFF',
+    padding: 16,
+    fontSize: 16,
   }
 })
